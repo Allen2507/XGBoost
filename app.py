43cfcb59122b76a523c7396adf4acbf91102874b
Allen <allen25072002@gmail.com>
2024-03-13 08:47:59 +0530

    Allen_b1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import pickle
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
@@ -12,9 +13,13 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def result():
-    try:
-        sku = int(request.form['SKU'])
+    
+        sku = str(request.form['SKU'])
+        label_encoder = LabelEncoder()
+        encoded_sku = label_encoder.transform([sku])
+        
         availability = int(request.form['Availability'])
+        stock_levels = int(request.form['Stock levels'])
         costs = float(request.form['Costs'])
         customer_demographics = int(request.form['Customer demographics'])
         defect_rates = float(request.form['Defect rates'])
@@ -28,17 +33,15 @@ def result():
         product_type = int(request.form['Product type'])
         production_volume = int(request.form['Production volumes'])
 
-        X= np.array([[sku, availability, costs, customer_demographics, 
+        X= np.array([[encoded_sku, availability, stock_levels, costs, customer_demographics, 
                       defect_rates, inspection_results, lead_time, lead_times, 
                       manufacturing_lead_time, count_of_products_sold, order_quantity, 
                       price, product_type, production_volume]])
 
-        X_standardized = md.transform(X)
-        y_prediction = md.predict(X_standardized)
+        
+        y_prediction = md.predict(X)
 
         return render_template('prediction.html', prediction=y_prediction)
-    except:
-        return 'Error: SKU field is missing in the form data', 400
 
 @app.route('/form')
 def form():
